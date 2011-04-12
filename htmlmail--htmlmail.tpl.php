569eedf58fb45f5d30754b1d5eb3c25a7de671f7
Bob Vincent <bobvin@pillars.net>
2011-04-12 15:51:39 -0400

    Fixed bug in htmlmail--htmlmail.tpl.php.

--- htmlmail--htmlmail.tpl.php
+++ htmlmail--htmlmail.tpl.php
@@ -17,8 +17,8 @@
   </p></dt><dd><ol><li><p>
     Copy
     <a href="http://drupalcode.org/project/htmlmail.git/blob/refs/heads/7.x-2.x:/htmlmail--htmlmail.tpl.php"><code>htmlmail--htmlmail.tpl.php</code></a>
-    to the <u><code><?php echo realpath($directory); ?></code></u>
-    directory of your <q><?php echo ucfirst($theme) ?> theme</q>.
+    to your <q><?php echo ucfirst($theme) ?> theme</q> directory
+    <u><code><?php echo $theme_path; ?></code></u>.
   </p></li></ul></p></li><li><p>
     Edit your copy of the <u><code>htmlmail--htmlmail.tpl.php</code></u> file.
   </p></li><ol>
