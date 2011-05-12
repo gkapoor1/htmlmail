90873501333a354aa754ca2faa9dab15822b141e
Bob Vincent <bobvin@pillars.net>
2011-05-12 10:21:35 -0400

    Issue #1155180 by adriaanm: Correct constant name from "__FILE" to "__FILE__"

--- htmlmail.tpl.php
+++ htmlmail.tpl.php
@@ -109,7 +109,7 @@
   $template_name = basename(__FILE__);
   $current_path = realpath(NULL);
   $current_len = strlen($current_path);
-  $template_path = realpath(dirname(__FILE));
+  $template_path = realpath(dirname(__FILE__));
   if (!strncmp($template_path, $current_path, $current_len)) {
     $template_path = substr($template_path, $current_len + 1);
   }
